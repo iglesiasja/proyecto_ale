0075005eccf96d16c17e38c4be0caf636e26e9de
Alejandro <joseaiglesiasc@gmail.com>
2020-02-13 16:04:03 -0400

    primer commit

---
+++ pages/forms.py
@@ -0,0 +1,17 @@
+from .models import Page
+from django import forms
+
+class PageForm (forms.ModelForm):
+
+    class Meta:
+        model=Page
+        fields=['title', 'content', 'order']
+        widgets={
+            'title':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Titulo'}),
+            'content':forms.Textarea(attrs={'class':'form-control'}),
+            'order':forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Orden'}),
+        }
+
+        labels = {
+            'title':'', 'content':'', 'order':''
+        }
